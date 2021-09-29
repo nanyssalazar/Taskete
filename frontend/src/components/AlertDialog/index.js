445636b1172729e0a0d2c892d8baa9b9717d56ad
andreassalazar <andrea.sosa01@iest.edu.mx>
2021-09-28 23:36:36 -0500

    Style alert, add keydown features for submitting form

--- frontend/src/components/AlertDialog/index.js
+++ frontend/src/components/AlertDialog/index.js
@@ -4,21 +4,21 @@ import './AlertDialog.scss';
 const AlertDialog = (props) => {
   return (
     <>
-      <div className='backdrop' />
-      <form className='form' onSubmit={(e) => props.onSubmit(e)}>
+      <div className="backdrop" onClick={props.onClose} />
+      <form className="alert" onSubmit={(e) => props.onSubmit(e)}>
         <p>{props.title}</p>
         <label>{props.message}</label>
-        <div className='form--actions'>
-          <button onClick={props.onClose} className='form__cancel'>
+        <div className="alert--actions">
+          <button onClick={props.onClose} className="alert__cancel">
             Cancel
           </button>
-          <button type='submit' className='form__add'>
+          <button type="submit" className="alert__confirm">
             {props.submitBtnMsg}
           </button>
         </div>
       </form>
     </>
-  )
+  );
 }
 
 export default AlertDialog;
